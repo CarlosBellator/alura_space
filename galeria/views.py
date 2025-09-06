4e69941cd9e40a42e11e19d27ab285b9ca31105d
Carlos Eduardo Dias Carneiro <carloseduardodiascarneiro2@gmail.com>
2025-09-06 18:39:18 -0300

    aula 2 arrumando painel admin

--- galeria/views.py
+++ galeria/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
 def index(request):
-    dados = {
-            1: {
-                'nome': 'Nebulósa de Carina',
-                'legenda': 'webtelescope.org / NASA / James Webb',
-            },
-            2: {
-                'nome': 'Exploração de Marte',
-                'legenda': 'webtelescope.org / NASA / Perseverance',
-            }
-    }
-    return render(request, 'galeria/index.html', {'cards': dados})
+    # Exemplo de dados:
+    # dados = {
+    #     1: {
+    #         'nome': 'Nebulósa de Carina',
+    #         'legenda': 'webtelescope.org / NASA / James Webb',
+    #     },
+    #     2: {
+    #         'nome': 'Exploração de Marte',
+    #         'legenda': 'webtelescope.org / NASA / Perseverance',
+    #     }
+    # }
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'cards': fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
