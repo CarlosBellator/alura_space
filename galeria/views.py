d7adfeb259b9bb2e5d080221de8d193c380e2bba
Carlos Eduardo Dias Carneiro <carloseduardodiascarneiro2@gmail.com>
2025-09-06 11:43:27 -0300

    aula 1 criação de banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+            1: {
+                'nome': 'Nebulósa de Carina',
+                'legenda': 'webtelescope.org / NASA / James Webb',
+            },
+            2: {
+                'nome': 'Exploração de Marte',
+                'legenda': 'webtelescope.org / NASA / Perseverance',
+            }
+    }
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
